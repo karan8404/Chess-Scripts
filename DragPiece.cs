e30b3c8577c0c2a8a79e6bc919d1973ce7e010a1
karan8404 <114310500+karan8404@users.noreply.github.com>
2023-02-12 16:13:23 +0530

    Drag and drop functionality

--- DragPiece.cs
+++ DragPiece.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class DragPiece : MonoBehaviour
 {
+    RaycastHit pickedPiece;
     Camera m_Camera;
+    GameObject dummyObject;
+    RaycastHit dummy;
     void Awake()
     {
         m_Camera = Camera.main;
     }
+    void Start(){
+        dummy=getDummy();
+        pickedPiece=dummy;
+    }
     void Update()
     {
         Mouse mouse = Mouse.current;
+        Vector3 mousePosition = mouse.position.ReadValue();
+
         if (mouse.leftButton.wasPressedThisFrame)
         {
-            Vector3 mousePosition = mouse.position.ReadValue();
             Ray ray = m_Camera.ScreenPointToRay(mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, 50))
-            {
-                Debug.Log(hit.point);
-                hit.transform.localScale = hit.transform.localScale * 1.2f;
+            if(Physics.Raycast(ray, out RaycastHit hit, 10)){
+                pickedPiece=hit;
             }
         }
+
+        if (mouse.leftButton.isPressed)
+        {
+            pickedPiece.transform.position = m_Camera.ScreenToWorldPoint(mousePosition)+new Vector3(0,0,10);
+        }
+
+        if(mouse.leftButton.wasReleasedThisFrame){
+            pickedPiece=dummy;
+        }
+    }
+
+    RaycastHit getDummy(){
+        dummyObject=new GameObject();
+        dummyObject.AddComponent<BoxCollider>();
+        dummyObject.transform.position=new Vector3(0,0,-5);
+        RaycastHit dummyHit;
+        Physics.Raycast(new Vector3(0,0,-10),Vector3.forward,out dummyHit);
+        return dummyHit;
     }
 }
